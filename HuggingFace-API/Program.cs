eea16e0dcd7cd37562212200047c7f96488f5b80
alonedelshtein <alonedelshtein@gmail.com>
2022-11-21 13:58:13 +0200

    Revert changes

--- HuggingFace-API/Program.cs
+++ HuggingFace-API/Program.cs
@@ -1,42 +1,34 @@
 using HuggingFace.API.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
-using Microsoft.AspNetCore.Authentication.Certificate;
 
 var builder = WebApplication.CreateBuilder(args);
 
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
-builder.Services.ConfigureCors(); 
+builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.AddAutoMapper(typeof(Program));
-//builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
-//    .AddCertificate();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 else
-{
-    //app.UseAuthentication();
-}
+    app.UseHsts();
+
 //app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseForwardedHeaders(new ForwardedHeadersOptions 
-{ 
-    ForwardedHeaders = ForwardedHeaders.All 
-}); 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
 
 app.UseCors("CorsPolicy");
 
